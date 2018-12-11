30b7964d8566c3a4c84e507b77add88cc1b522f6
Hung Nguyen <nmhung1805@gmail.com>
2018-12-11 17:45:34 +0700

    Use new URL parser instead of old one

--- utils/db.js
+++ utils/db.js
@@ -2,7 +2,7 @@ var mongoose = require('mongoose');
 
 const mlabURI = 'mongodb://admin-cinema:111111a@ds121624.mlab.com:21624/cinema'
 
-const con = mongoose.connect(mlabURI, (error) => {
+const con = mongoose.connect(mlabURI,{ useNewUrlParser: true }, (error) => {
 	if(error){
 		console.log("Error " + error);
 	}else{
